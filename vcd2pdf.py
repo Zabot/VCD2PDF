2c3c96aa48f89be818ee363c21a38ee04e7105af
Zach Anderson <zach.inbox@gmail.com>
2016-11-24 20:14:59 -0600

    Added support for invalid states

--- vcd2pdf.py
+++ vcd2pdf.py
@@ -3,6 +3,7 @@
 import sys;
 import re;
 from collections import namedtuple;
+from collections import defaultdict;
 
 from pyx import *;
 
@@ -29,7 +30,8 @@ SCOPE_REGEX = "\$((?:up)?scope)(?:\s+\$end|\s+(module)\s+(\w+)\s+\$end\s+((?:.(?
 # CG2 - Number of bits
 # CG3 - ASCII shorthand
 # CG4 - variable name
-VAR_REGEX = "\$var\s+(wire|reg)\s+(\d)\s+(.)\s+(\w+)\s+\$end";
+# CG5 - bus bits
+VAR_REGEX = "\$var\s+(wire|reg)\s+(\d)\s+(.)\s+(\w+)\s+(?:\[(\d+:\d+)\]\s+)?\$end";
 
 # CG1 - Time index
 # CG2 - Variables that changed
@@ -37,8 +39,7 @@ TIME_REGEX = "#(\d+)\s*((?:[\db]+\s*[!-~]\s*)*)";
 
 # CG1 - New value
 # CG2 - Varaible
-VALUE_REGEX = "(\w+)\s*([!-~])";
-
+STATE_REGEX = "([\dxX]|[bB][0-9xX]+\s+)([!-~])";
 
 with open(sys.argv[1], 'r') as myfile:
     data=myfile.read();
@@ -59,61 +60,97 @@ timescale = result.group(3);
 scopes = re.findall(SCOPE_REGEX, result.group(4), re.DOTALL);
 
 # Grab all of the variables
-Variable = namedtuple("Variable", ["scope", "name", "ascii", "changes"]);
+Variable = namedtuple("Variable", ["scope", "name", "ascii", "bits", "states"]);
 
-variables = {};
+variables = defaultdict(list);
 activeScope = [];
 for scope in scopes:
     if scope[0] == "scope":
         activeScope.append(scope[2]);
         variableText = re.findall(VAR_REGEX, scope[3]);
         for var in variableText:
-            variables[var[2]] = Variable(activeScope[-1], var[3], var[2], []);
+            variables[var[2]].append(Variable(activeScope[-1], var[3], var[2], int(var[1]), []));
 
     elif scope[0] == "upscope":
         activeScope.pop();
 
+def parseState(startTime, stateMatch):
+    if stateMatch[0].lower().startswith('b'):
+        valueString = stateMatch[0][1:];
 
-# Grab the initial values
-init = result.group(5);
-values = re.findall(VALUE_REGEX, init);
+        # Value is a valid binary number
+        try:
+            value = int(stateMatch[0][1:], 2);
 
-for value in values:
-    variables[value[1]].changes.append((0, value[0]));
+        # Value is not binary number, bits are floating or error
+        except:
+            value = valueString;
+
+    else:
+        try:
+            value = int(stateMatch[0]);
+
+        except:
+            value = stateMatch[0];
+
+    return State(startTime, value);
 
 
-# Grab all of the changes
+# Grab the initial states
+State = namedtuple("State", ["startTime", "value"]);
+
+initialStates = re.findall(STATE_REGEX, result.group(5));
+
+for stateMatch in initialStates:
+    for var in variables[stateMatch[1]]:
+        var.states.append( parseState(0, stateMatch) );
+    
+
+# Grab all of the states
 last = 0
 steps = re.findall(TIME_REGEX, result.group(6), re.DOTALL);
 for step in steps:
     last = max(last, int(step[0]));
-    deltas = re.findall(VALUE_REGEX, step[1]);
-    for delta in deltas:
-        variables[delta[1]].changes.append((int(step[0]), delta[0]));
+    states = re.findall(STATE_REGEX, step[1]);
+    for stateMatch in states:
+        for var in variables[stateMatch[1]]:
+            var.states.append( parseState(int(step[0]), stateMatch) );
 
 
 # Add an end state to all of the variables
-for key, var in variables.items():
-    var.changes.append((last, ''));
-
+for key, varList in variables.items():
+    for var in varList:
+        var.states.append( State(last, '') );
 
 # Draw the waveforms
 c = canvas.canvas();
 
-i = 0;
-high = 0.422;
-xscale = 0.15;
 
-for key, var in variables.items():
-    y = i;
-    c.text(-0.2, y + (0.422 / 2.0), var.name.replace("_","\_"), [text.halign.boxright, text.valign.middle]);
-    waveform = path.path(path.moveto(0, y + int(var.changes[0][1]) * high));
-    for j in range(1, len(var.changes) - 1):
-        waveform.append(path.lineto(xscale * var.changes[j][0], y + int(var.changes[j-1][1]) * high));
-        waveform.append(path.lineto(xscale * var.changes[j][0], y + int(var.changes[j][1]) * high));
-    waveform.append(path.lineto(xscale * var.changes[-1][0], y + int(var.changes[-2][1]) * high));
-    c.stroke(waveform, []);
-    i += 1;
+def drawWavePath(variable, canvas, x, y, xscale, yscale):
+    states = variable.states;
+
+    for j in range(0, len(var.states) - 1):
+        if variable.bits == 1:
+            if states[j].value == 'x':
+                canvas.stroke(path.rect(x + states[j].startTime * xscale, y, x + (states[j + 1].startTime - states[j].startTime) * xscale, yscale), [color.rgb.black, deco.filled([color.cmyk.Gray])]);
+
+            else:
+                canvas.stroke(path.line(x + states[j].startTime * xscale, y + states[j].value * yscale, x + states[j+1].startTime * xscale, y + states[j].value * yscale));
+                if states[j + 1].value != '':
+                    canvas.stroke(path.line(x + states[j + 1].startTime * xscale, y + states[j].value * yscale, x + states[j + 1].startTime * xscale, y + states[j + 1].value * yscale));
+        else:
+            pass;
+            # Draw bus
+
+
+y = 0;
+yscale = 0.422;
+xscale = 0.15;
+for key, varList in variables.items():
+    for var in varList:
+        c.text(-0.2, y + (0.422 / 2.0), var.scope + "::" + var.name.replace("_","\_"), [text.halign.boxright, text.valign.middle]);
+        drawWavePath(var, c, 0, y, xscale, yscale);
+        y += 1;
 
 # Fill and trim the canvas
 bounds = c.bbox().enlarged(0.5);
